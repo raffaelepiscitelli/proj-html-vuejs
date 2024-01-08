b5fe249bc5e593f38f65185204e082ecda6596c4
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2024-01-08 11:37:09 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <AppHeader/>
+  <AppMain/>
+  <AppFooter/>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
+</script>
+<style lang="scss">
+  @use './style/general.scss';
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+    @use '../style/general.scss' as *;
+    @use '../style/partials/mixins' as *;
+    @use '../style/partials/variables' as *;
+</style>
