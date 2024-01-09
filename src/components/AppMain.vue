639234fc6da5c0d2b4f0ed67d9eb80051ccdfd41
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2024-01-09 16:53:38 +0100

    Completamento section specialty-services AppMain.vue

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -26,6 +26,77 @@
         </div>
 
     </section>
+
+    <section class="specialty-services">
+
+        <div class="speciality-services-container">
+
+            <div class="flex">
+
+                <h2>Specialty Services</h2>
+
+                <span class="bar"></span>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>Laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+            </div>
+
+            <div class="flex">
+
+                <section class="business-loans">
+
+                    <div class="business-loans-container">
+
+                        <i class="fa-solid fa-globe" style="color: #4a75ac;"></i>
+
+                        <h3>Small Business Loans</h3>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                        <span>Learn More</span>
+
+                    </div>
+
+                </section>
+
+                <section class="credit-rating-advice">
+
+                    <div class="credit-rating-advice-container">
+
+                        <i class="fa-regular fa-gem" style="color: #4a75ac;"></i>
+
+                        <h3>Credit Rating Advice</h3>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                        <span>Learn More</span>
+
+                    </div>
+
+                </section>
+
+                <section class="fast-crowd-funding">
+
+                    <div class="fast-crowd-funding-container">
+
+                        <i class="fa-solid fa-chart-line" style="color: #4a75ac;"></i>
+
+                        <h3>Fast Crowd Funding</h3>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                        <span>Learn More</span>
+
+                    </div>
+                    
+
+                </section>
+
+            </div>
+
+        </div>
+
+    </section>
 </template>
 <script>
 export default {
