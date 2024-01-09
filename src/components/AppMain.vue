8b50ca3e11daf694055b408b4413437c55ecfd99
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2024-01-09 11:37:09 +0100

    Completamento section cloud AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,27 @@
 <template>
     <section class="cloud">
         
-        <div class="cloud-container">
+        <div class="cloud-container flex">
+
+            <div class="flex">
+
+                <h2>Cloud Based Finance</h2>
+
+                <span class="bar"></span>
+                
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                <p>Laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+
+                <span>Learn more</span>
+
+            </div>
+
+            <div>
+
+                <img src="../assets/imgs/graph.jpg" alt="">
+
+            </div>
 
         </div>
 
