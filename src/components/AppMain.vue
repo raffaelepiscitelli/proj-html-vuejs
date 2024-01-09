14d563c3e72ce34bf8895f7a36da70706be2660f
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2024-01-09 23:11:38 +0100

    Completamento section client-feedback AppMain.vue

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -89,8 +89,118 @@
 
                     </div>
                     
-
                 </section>
+
+            </div>
+
+        </div>
+
+    </section>
+
+    <section class="investment-numbers">
+
+        <div class="investment-numbers-container flex">
+
+            <div class="flex">
+
+                <div class="flex">
+
+                    <p>Investment Numbers</p>
+
+                    <h2>Making An Impact In Our Communities</h2>
+
+                    <span>Learn More</span>
+
+                </div>
+
+            </div>
+
+            <div class="flex">
+
+
+            </div>
+
+        </div>
+
+    </section>
+
+    <section class="client-feedback">
+
+        <div class="client-feedback-container">
+
+            <div class="flex">
+
+                <h2>Client Feedback</h2>
+
+                <span class="bar"></span>
+
+            </div>
+
+            <div class="flex">
+
+                <div>
+
+                    <img src="../assets/imgs/client-logo-4.png" alt="">
+
+                    <p>"Avada Finance offers a fantastic service! It is so easy to use and the customer support is the best around. This payment option is my number one choice. I always recommend it."</p>
+                    
+                </div>
+
+                <div>
+
+                    <img src="../assets/imgs/client-logo-9.png" alt="">
+
+                    <p>"Avada Finance offers a fantastic service! It is so easy to use and the customer support is the best around. This payment option is my number one choice. I always recommend it."</p>
+                    
+                </div>
+
+                <div>
+
+                    <img src="../assets/imgs/client-logo-2.png" alt="">
+
+                    <p>"Avada Finance offers a fantastic service! It is so easy to use and the customer support is the best around. This payment option is my number one choice. I always recommend it."</p>
+                    
+                </div>
+
+                <div>
+
+                    <img src="../assets/imgs/client-logo-3.png" alt="">
+
+                    <p>"Avada Finance offers a fantastic service! It is so easy to use and the customer support is the best around. This payment option is my number one choice. I always recommend it."</p>
+                    
+                </div>
+
+                <div>
+
+                    <span>Andre Mendoza</span>
+
+                    <span>Creative Market</span>
+                    
+                </div>
+
+                <div>
+
+                    <span>Andre Mendoza</span>
+
+                    <span>Creative Market</span>
+                    
+                </div>
+
+                <div>
+
+                    <span>Andre Mendoza</span>
+
+                    <span>Creative Market</span>
+                    
+                </div>
+
+                <div>
+
+                    <span>Andre Mendoza</span>
+
+                    <span>Creative Market</span>
+                    
+                </div>
 
             </div>
 
