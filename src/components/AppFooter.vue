1b24f9bd135b21fbda71eecb31bef47f722ed428
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2024-01-10 11:49:31 +0100

    Completamento provvisorio up-footer AppFooter.vue

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,65 @@
 <template>
-    <div>
-        
-    </div>
+    <footer>
+
+        <div class="footer-container">
+
+            <div class="up-footer flex">
+
+                <div class="flex">
+
+                    <img src="../assets/imgs/finance_logo_footer.png" alt="">
+
+                </div>
+
+                <div class="flex">
+
+                    <span>Recent post</span>
+
+                    <ul>
+
+                        <li>> When is it the rigth time to sell your company?</li>
+
+                        <li>> Personal debt or company debt we explore your options</li>
+
+                        <li>> An interview with a so-called business angel</li>
+
+                    </ul>
+
+                </div>
+
+                <div class="flex">
+
+                    <span>Services</span>
+
+                    <ul>
+
+                        <li>> Small business loan services</li>
+
+                        <li>> Credit rating advice services</li>
+
+                        <li>> Crowd funding services</li>
+
+                    </ul>
+
+                </div>
+
+                <div class="flex">
+
+                    <span>Latest</span>
+
+                    <i class="fa-brands fa-twitter" style="color: #5aaeeb;"></i>
+
+                </div>
+
+            </div>
+
+            <div class="bottom-footer">
+
+            </div>
+
+        </div>
+
+    </footer>
 </template>
 <script>
 export default {
